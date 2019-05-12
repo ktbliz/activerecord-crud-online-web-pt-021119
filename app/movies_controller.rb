10ed83ab1c7281a348c95c533f802e3c30ce586e
Katie Blizzard <kblizzard11@gmail.com>
2019-05-12 01:45:58 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -47,12 +47,12 @@ def can_find_the_first_item_from_the_database_using_id
   Movie.find(1)
 end
 
-def can_find_by_multiple_attributes(attributes)
+def can_find_by_multiple_attributes
   # Search Values:
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  Movie.find_by(attributes)
+  Movie.find_by(:title)
 end
 
 def can_find_using_where_clause_and_be_sorted
